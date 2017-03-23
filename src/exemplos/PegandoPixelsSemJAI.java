bb9f9bb8876fca17afe4065991bc05e781a5f32a
ALuiz <andreluiz.cssantos@gmail.com>
2017-03-23 17:43:45 -0300

    Merge origin/master
    Conflicts:
    	nbproject/project.properties

--- src/exemplos/PegandoPixelsSemJAI.java
+++ src/exemplos/PegandoPixelsSemJAI.java
@@ -5,35 +5,90 @@
  */
 package exemplos;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.statistics.HistogramDataset;
+import org.jfree.data.statistics.HistogramType;
 
 /**
  *
  * @author andre
  */
-public class PegandoPixelsSemJAI {
+public class PegandoPixelsSemJAI extends JFrame {
     
-    public static void main(String[] args) throws IOException{
+ 
+    
+    public void pegaPixel() throws IOException{
         
-        File f = new File("D:\\ProjetosNetBeans\\PDI\\src\\imagens\\checkerboard.png"); //seleciona o arquivo
+        File f = new File("D:\\ProjetosNetBeans\\PDI\\src\\imagens\\ebola.png"); //seleciona o arquivo
         BufferedImage image = ImageIO.read(f); //le o arquivo
         System.out.println("Dimensões: "+image.getWidth()+"x"+image.getHeight()+"pixels"); //exibe suas dimensões
         
         Raster raster = image.getRaster(); //pega os valores dos pixels
-        int[] pixel = new int[3]; //cria um array com 3 posições
+        double pixel[] = new double[3]; //cria um array com 3 posições
         int brancos = 0; //cria uma variável para contar os pixels brancos
         for(int h = 0; h<image.getHeight(); h++)
             for(int w = 0; w<image.getWidth(); w++){
                 raster.getPixel(w, h, pixel);
                 if((pixel[0] == 255) && (pixel[1] == 255) && (pixel[2] == 255))//confirma se o RGB é igual a 255, ou seja, branco
-                    brancos++;
+                brancos++;
+                
             }
         
-        System.out.println(brancos+" pixels brancos");
+       
     }
     
+    
+//    public ChartPanel criaHistograma(double[] histograma){
+//        // int number = data.length;
+//        dataset = new HistogramDataset();
+//        dataset.setType(HistogramType.RELATIVE_FREQUENCY);
+//        dataset.addSeries("Hist", histograma, 256);
+//        String plotTitle = "";
+//        String xAxis = "Frequency";
+//        String yAxis = "Mass Error (Da)";
+//        PlotOrientation orientation = PlotOrientation.VERTICAL;
+//
+//        boolean show = false;
+//        boolean toolTips = false;
+//        boolean urls = false;
+//        JFreeChart chart = ChartFactory.createHistogram(plotTitle, xAxis, yAxis,
+//                dataset, orientation, show, toolTips, urls);
+//
+//        chart.setBackgroundPaint(Color.white);
+//        ChartPanel panel = new ChartPanel(chart);
+//        panel.setMouseWheelEnabled(true);
+//        return panel;
+//    }
+//    
+//    public void montaTela() throws IOException{
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        add(pegaPixel(), BorderLayout.CENTER);
+//        pack();
+//        setLocationRelativeTo(null);
+//        setVisible(true);
+//    }
+//    
+//    public static void main(String args[]){
+//        EventQueue.invokeLater(() -> {
+//            try {
+//                new PegandoPixelsSemJAI().montaTela();
+//            } catch (IOException ex) {
+//                Logger.getLogger(PegandoPixelsSemJAI.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        });
+//    }
 }
--- src/pdi/Main.java
+++ src/pdi/Main.java
@@ -1,19 +1,87 @@
 package pdi;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 /**
  *
  * @author andre
  */
+
+/*
+* equação:
+* q = (En / x) -1, onde q são os novos niveis de tons da imagen,
+* n é a quantidade de vezes que o tom aparece, E é a somatoria de tons da imagem original
+* x = (h * w) / 256, onde h é a largura, w é a altura
+ */
+
 public class Main {
 
-    public static void main(String[] args) {
-        TelaPrincipal tp = new TelaPrincipal();
-//        tp.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        tp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        tp.setSize(800,600);
-        tp.setVisible(true);
+    private static File arq = new File("D:\\ProjetosNetBeans\\PDI\\src\\imagens\\ebola.png");
+
+    //método para pegar imagem
+    public BufferedImage pegaImagem() {
+        try {
+            return ImageIO.read(arq);
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+        return null;
     }
-    
+
+    public int[] pegaPixels(File file) {
+        BufferedImage img = pegaImagem();
+        int[] somaR = new int[256]; 
+        int[] somaG = new int[256]; 
+        int[] somaB = new int[256]; 
+        
+        int[] qR = new int[256]; 
+        int[] qG = new int[256]; 
+        int[] qB = new int[256]; 
+        
+        int w = img.getWidth();
+        int h = img.getHeight();
+        int x = (w*h) / 256;
+        int[] rgb = new int[w * h];
+
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                rgb[i * j] = img.getRGB(i, j);
+            }
+        }
+
+        for (int i = 0; i < rgb.length; i++) {
+            Color c = new Color(rgb[i]);
+            if(i == 0){
+                somaR[i] = c.getRed();
+                somaG[i] = c.getGreen();
+                somaB[i] = c.getBlue();
+            } else {
+                somaR[i] = c.getRed()   + somaR[i - 1];
+                somaG[i] = c.getGreen() + somaG[i - 1];;
+                somaB[i] = c.getBlue()  + somaB[i - 1];;
+            }
+            qR[i] = (somaR[i] / x) - 1;
+            if(qR[i] < 0)
+                qR[i] = 0;
+            qG[i] = (somaG[i] / x) - 1;
+            if(qG[i] < 0)
+                qG[i] = 0;
+            qB[i] = (somaB[i] / x) - 1;
+            if(qB[i] < 0)
+                qB[i] = 0;
+            
+        }
+        return rgb;
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.pegaPixels(arq);
+    }
+
 }
